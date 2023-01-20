770e09767ca3e4b830008446682e3822288ad4a0
Raushan kr <44736489+raushnn@users.noreply.github.com>
2023-01-21 02:59:59 +0530

    Create problem2.c

---
+++ assgn2/problem2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]){
+    
+    for (int i=1; i<argc; i++){
+        if (fork()==0){
+            if (i+1<argc && argv[i+1][0]=='-'){
+                char *param[]={argv[i],argv[i+1], NULL};
+                execvp(argv[i], param);
+                ++i;
+            }
+            else{
+                execvp(argv[i], NULL);
+                ++i;
+            }
+            printf("%d", getpid());
+        }
+        wait(NULL);
+
+    }
+    return 0;
+}
